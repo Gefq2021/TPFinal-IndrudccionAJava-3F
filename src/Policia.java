75a3104207171147e8313e08b3479290eaa8eacc
Gerardo Fabian Quispe <gfq0684@gmail.com>
2024-10-19 09:49:40 -0300

    Creamos la clase Policia

---
+++ src/Policia.java
@@ -0,0 +1,40 @@
+public class Policia {
+    private String nombre;
+    private String apellido;
+    private int legajo;
+
+    public Policia(String nombre, String apellido, int legajo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    @Override
+    public String toString() {
+        return "Policia:\n Nombre: " + this.nombre + "\n Apellido: " + this.apellido + "\n Legajo: " + this.legajo;
+    }
+}
