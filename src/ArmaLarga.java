2621f19ee837eb2d5fca51ef407947d9a4778b68
Gerardo Fabian Quispe <gfq0684@gmail.com>
2024-10-20 14:16:37 -0300

    Cramos la clase ArmaLarga

--- src/ArmaLarga.java
+++ src/ArmaLarga.java
@@ -1,2 +1,17 @@
-public class ArmaLarga {
+public class ArmaLarga extends Arma {
+    private boolean selloRENAR;
+    private String descripcionUso;
+    private int nivel;
+
+    public ArmaLarga(int cantMuniciones, double alcance, String marca, int calibre, String estado, boolean selloRENAR, String descripcionUso, int nivel) {
+        super(cantMuniciones, alcance, marca, calibre, estado);
+        this.selloRENAR = selloRENAR;
+        this.descripcionUso = descripcionUso;
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String getTipoArma() {
+        return "Larga";
+    }
 }
